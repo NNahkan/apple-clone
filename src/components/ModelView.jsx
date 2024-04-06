749e5ba57a8940d13244d66fa6a540b9dab49929
NNahkan <hoksuz2424@gmail.com>
2024-04-06 00:05:15 -0400

    Figuring out model

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import { PerspectiveCamera, View } from "@react-three/drei";
+import Lights from "./Lights";
+import IPhone from "./IPhone";
+import { Suspense } from "react";
 
-const ModelView = () => {
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationSize,
+  size,
+  item,
+}) => {
   return (
-	 <div>
-		modelview
-	 </div>
-  )
-}
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full ${
+        index === 2
+      } ? 'right-[-100%] : ''`}
+    >
+      <ambientLight intensity={0.3} />
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <Lights />
+      <Suspense fallback={<div>Loading</div>}>
+        <IPhone />
+      </Suspense>
+    </View>
+  );
+};
 
-export default ModelView
+export default ModelView;
